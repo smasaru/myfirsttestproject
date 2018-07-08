129daae86b159b2f4b2b3acf2554711347910943
smasaru <s.masaru.dev@gmail.com>
2018-07-08 16:28:55 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 class Test {
   public static void main(String[] args) {
-    System.out.println("Hello again Java");
+    int testint = 0;
+    System.out.println("Hello again Java! " + testint);
   }
 }
